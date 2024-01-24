a2fbd986778e86fb385d18b7ed6900bc7a11be3d
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-01-24 14:17:21 +0100

    creating the voting application source code files

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/VotingApplication.java
@@ -0,0 +1,13 @@
+package com.ingryd_capstone_project.votingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingApplication.class, args);
+	}
+
+}
