63d8b1166e70a751d6aa98cf8dc751c758ba64ab
Mary Adewunmi <physayoemma@gmail.com>
2024-01-26 19:42:43 +0100

    created the vote's repository

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.ingryd_capstone_project.votingapplication.repository;
+
+import com.ingryd_capstone_project.votingapplication.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
