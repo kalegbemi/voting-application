7b30c75cde4bbf226d5aad7b7c38a6f09be3570a
INVESTHOR365 <azeezyusuf2111@gmail.com>
2024-01-31 14:01:16 +0100

    Naming convention modification

--- src/main/java/com/ingryd_capstone_project/votingapplication/model/Voter.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/model/Voter.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Getter
 @Entity
 @Table(name = "voter_table")
-public class Voters {
+public class Voter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
