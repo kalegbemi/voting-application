b3b855a4e263b9e15afb4dabf119cc3c5479399b
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-01 22:46:36 +0100

    added mail dependency to the branch and also modified message util from private to public methods

--- src/main/java/com/ingryd_capstone_project/votingapplication/util/MessageUtil.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/util/MessageUtil.java
@@ -4,12 +4,12 @@ import com.ingryd_capstone_project.votingapplication.dto.CandidateEmailDetails;
 
 public class MessageUtil {
 
-    private static String getVoterMessage(String to, String name, String role){
+    public static String getVoterMessage(String to, String name, String role){
         return "Hello "+ name+",\n\nYou have been registered as a "+role+" on the INGRYD VOTING APPLICATION." +
                 "\n\nFurther information will be communicated as the needs arises.\n\n\n\nThank you,\n\nSupport team.";
     }
 
-    private static String getCandidateMessage(CandidateEmailDetails details){
+    public static String getCandidateMessage(CandidateEmailDetails details){
         return "Hello "+ details.getName()+",\n\nYou have been registered as a "+details.getRole()+" on the INGRYD VOTING APPLICATION." +
                 "\n\nYou were registered as a candidate contesting for the position of "+details.getPosition() +"under" +
                 "the party "+details.getParty()+"."+
