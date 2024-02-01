068cbf934d39aca36ef258ef04ef5bcc36283263
Mary Adewunmi <physayoemma@gmail.com>
2024-02-01 05:04:52 +0100

    Created an exception class that extends RunTimeException

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/exception/VoteNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ingryd_capstone_project.votingapplication.exception;
+
+public class VoteNotFoundException extends RuntimeException {
+
+    public VoteNotFoundException(String message) {
+        super(message);
+    }
+
+}
