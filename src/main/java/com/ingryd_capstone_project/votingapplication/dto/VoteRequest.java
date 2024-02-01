b00ce30357416c5df731619065d0e93e91fb1505
Mary Adewunmi <physayoemma@gmail.com>
2024-02-01 08:46:46 +0100

    created voteDto class

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/dto/VoteRequest.java
@@ -0,0 +1,18 @@
+package com.ingryd_capstone_project.votingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VoteRequest {
+
+    private Voter voterId;
+    private Candidate candidateId;
+    private Election electionId;
+
+}
