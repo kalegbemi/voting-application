709eabcaafc2c53679fd4ef7a9433e0a80e16cbc
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-01 21:50:20 +0100

    added the default message after registration

--- src/main/java/com/ingryd_capstone_project/votingapplication/util/MessageUtil.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/util/MessageUtil.java
@@ -1,4 +1,18 @@
 package com.ingryd_capstone_project.votingapplication.util;
 
+import com.ingryd_capstone_project.votingapplication.dto.CandidateEmailDetails;
+
 public class MessageUtil {
+
+    private static String getVoterMessage(String to, String name, String role){
+        return "Hello "+ name+",\n\nYou have been registered as a "+role+" on the INGRYD VOTING APPLICATION." +
+                "\n\nFurther information will be communicated as the needs arises.\n\n\n\nThank you,\n\nSupport team.";
+    }
+
+    private static String getCandidateMessage(CandidateEmailDetails details){
+        return "Hello "+ details.getName()+",\n\nYou have been registered as a "+details.getRole()+" on the INGRYD VOTING APPLICATION." +
+                "\n\nYou were registered as a candidate contesting for the position of "+details.getPosition() +"under" +
+                "the party "+details.getParty()+"."+
+                "\n\nFurther information will be communicated as the needs arises.\n\n\n\nThank you,\n\nSupport team.";
+    }
 }
