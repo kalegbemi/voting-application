696bb29b774202a2da0786377cb312b884b827c6
Mary Adewunmi <physayoemma@gmail.com>
2024-02-02 17:34:07 +0100

    modified

--- src/main/java/com/ingryd_capstone_project/votingapplication/dto/VoteRequest.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/dto/VoteRequest.java
@@ -1,18 +1,11 @@
 package com.ingryd_capstone_project.votingapplication.dto;
 
-import lombok.AllArgsConstructor;
+import com.ingryd_capstone_project.votingapplication.model.Candidate;
+import com.ingryd_capstone_project.votingapplication.model.Election;
+import com.ingryd_capstone_project.votingapplication.model.Voter;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class VoteRequest {
-
-    private Voter voterId;
-    private Candidate candidateId;
-    private Election electionId;
-
-}
+public record VoteRequest(
+        Voter voterId, Candidate candidateId, Election electionId
+){}
