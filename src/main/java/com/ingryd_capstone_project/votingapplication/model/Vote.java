6bdb0bed9b9f28f805f71429bac63713e36407c4
Ayokunle Lawal <ayokunle.lawal@yahoo.com>
2024-02-03 09:35:58 +0100

    Edited the application config class

--- src/main/java/com/ingryd_capstone_project/votingapplication/model/Vote.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/model/Vote.java
@@ -1,41 +1,41 @@
-package com.ingryd_capstone_project.votingapplication.model;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.engine.internal.Cascade;
-
-import java.time.LocalDateTime;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Entity
-public class Vote {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
-
-    @ManyToOne(cascade =CascadeType.ALL)
-    private Voter voter;
-
-    @ManyToOne
-   private Candidate candidate;
-
-    @ManyToOne
-   private Election election;
-
-    private LocalDateTime startTime;
-
-    private LocalDateTime endTime;
-
-
-
-
-
-
-}
+//package com.ingryd_capstone_project.votingapplication.model;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.engine.internal.Cascade;
+//
+//import java.time.LocalDateTime;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@Entity
+//public class Vote {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//   private Long id;
+//
+//    @ManyToOne(cascade =CascadeType.ALL)
+//    private Voter voter;
+//
+//    @ManyToOne
+//   private Candidate candidate;
+//
+//    @ManyToOne
+//   private Election election;
+//
+//    private LocalDateTime startTime;
+//
+//    private LocalDateTime endTime;
+//
+//
+//
+//
+//
+//
+//}
--- src/main/java/com/ingryd_capstone_project/votingapplication/service/VoteService.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/service/VoteService.java
@@ -1,32 +1,32 @@
-package com.ingryd_capstone_project.votingapplication.service;
-
-import com.ingryd_capstone_project.votingapplication.model.Election;
-import com.ingryd_capstone_project.votingapplication.model.Vote;
-import com.ingryd_capstone_project.votingapplication.repository.VoteRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class VoteService {
-
-    private VoteRepository voteRepository;
-    public ResponseEntity<Vote> getElectionById(Long electionId) {
-       return new ResponseEntity<>(voteRepository.findById(electionId).get(), HttpStatus.OK);
-    }
-
-    public ResponseEntity<Vote> getVoteByVoterId(long voterId) {
-        return new ResponseEntity<>(voteRepository.findByVoterId(voterId), HttpStatus.OK);
-    }
-
-
-    public ResponseEntity<List<Vote>> findAllVote() {
-
-        return new ResponseEntity<>(voteRepository.findAll(),HttpStatus.OK);
-    }
-}
+//package com.ingryd_capstone_project.votingapplication.service;
+//
+//import com.ingryd_capstone_project.votingapplication.model.Election;
+//import com.ingryd_capstone_project.votingapplication.model.Vote;
+//import com.ingryd_capstone_project.votingapplication.repository.VoteRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class VoteService {
+//
+//    private VoteRepository voteRepository;
+//    public ResponseEntity<Vote> getElectionById(Long electionId) {
+//       return new ResponseEntity<>(voteRepository.findById(electionId).get(), HttpStatus.OK);
+//    }
+//
+//    public ResponseEntity<Vote> getVoteByVoterId(long voterId) {
+//        return new ResponseEntity<>(voteRepository.findByVoterId(voterId), HttpStatus.OK);
+//    }
+//
+//
+//    public ResponseEntity<List<Vote>> findAllVote() {
+//
+//        return new ResponseEntity<>(voteRepository.findAll(),HttpStatus.OK);
+//    }
+//}
