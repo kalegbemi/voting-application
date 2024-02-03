fadc67d7a646c0b7b4b0303a5f4d801d58fb2c37
INVESTHOR365 <azeezyusuf2111@gmail.com>
2024-02-03 06:19:24 +0100

    service class modify

--- src/main/java/com/ingryd_capstone_project/votingapplication/service/VoterService.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/service/VoterService.java
@@ -6,6 +6,8 @@ import com.ingryd_capstone_project.votingapplication.repository.VoterRepository;
 import com.ingryd_capstone_project.votingapplication.request.UserRegisterationRequest;
 import com.ingryd_capstone_project.votingapplication.request.VoterUpdateRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,22 +17,23 @@ import java.util.Optional;
 public class VoterService {
 
     private final VoterRepository voterRepository;
-
+    @Cacheable
     public List<Voter> getAllVoters() {
 
         return voterRepository.findAll();
     }
-
+    @Cacheable
     public Optional<Voter> getVoterById(long voterId) {
 
         return voterRepository.findById(voterId);
     }
 
+    @CacheEvict
     public Voter createVoter(UserRegisterationRequest voter) {
 
         return voterRepository.createVoter(voter);
     }
-
+    @CacheEvict
     public void deleteVoter(long voterId) {
 
         voterRepository.deleteById(voterId);
@@ -39,8 +42,9 @@ public class VoterService {
         Voter voter = voterRepository.findByUsername(username);
         return voter != null && voter.getPassword().equals(password);
     }
-
+    @CacheEvict
     public Voter updateVoter(long id, VoterUpdateRequest updateRequest) {
+
         return voterRepository.updateVoter();
     }
 }
