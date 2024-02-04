667d415ed5e8505b007c729aa8bb225825adedd3
Mary Adewunmi <physayoemma@gmail.com>
2024-02-04 15:16:34 +0100

    created electionResult entity and the repository extending JapRepository

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/repository/ElectionResultRepository.java
@@ -0,0 +1,7 @@
+package com.ingryd_capstone_project.votingapplication.repository;
+
+import com.ingryd_capstone_project.votingapplication.model.ElectionResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ElectionResultRepository extends JpaRepository<ElectionResult> {
+}
