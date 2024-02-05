964858f3e87814e71a2b4204a427c87691f7cc22
Mary Adewunmi <physayoemma@gmail.com>
2024-02-05 11:24:46 +0100

    created a method to get the total votes of a particular candidate

---
+++ src/main/java/com/ingryd_capstone_project/votingapplication/service/ElectionResultService.java
@@ -0,0 +1,22 @@
+package com.ingryd_capstone_project.votingapplication.service;
+
+import com.ingryd_capstone_project.votingapplication.model.Vote;
+import com.ingryd_capstone_project.votingapplication.repository.ElectionResultRepository;
+import com.ingryd_capstone_project.votingapplication.repository.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ElectionResultService {
+
+    private final ElectionResultRepository resultRepository;
+    private final VoteRepository voteRepository;
+
+    public int getTotalVotesForCandidate(Candidate candidate) {
+        List<Vote> results = voteRepository.findByCandidate(candidate);
+        return results.size();
+    }
+}
