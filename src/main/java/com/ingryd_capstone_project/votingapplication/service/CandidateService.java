a2435abf81b51565734dc86fbe610520deab25f6
INVESTHOR365 <azeezyusuf2111@gmail.com>
2024-02-05 11:46:24 +0100

    candidateupdaterequest and candidateservice completion

--- src/main/java/com/ingryd_capstone_project/votingapplication/service/CandidateService.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/service/CandidateService.java
@@ -1,7 +1,9 @@
 package com.ingryd_capstone_project.votingapplication.service;
 
 import com.ingryd_capstone_project.votingapplication.model.Candidate;
+import com.ingryd_capstone_project.votingapplication.model.Voter;
 import com.ingryd_capstone_project.votingapplication.repository.CandidateRepository;
+import com.ingryd_capstone_project.votingapplication.request.CandidateUpdateRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 @AllArgsConstructor
 @Service
 public class CandidateService {
@@ -20,6 +20,7 @@ public class CandidateService {
    private final CandidateRepository candidateRepository;
 
    public long getid() {
+
       return candidateRepository.getId();
    }
    @CacheEvict(value = "allCandidate", allEntries = true)
@@ -42,9 +43,30 @@ public class CandidateService {
    }
 
    public Candidate getCandidateById(long id) {
+
       return candidateRepository.findById(id).orElse(null);
    }
 
+   public String updateCandidate(long id, CandidateUpdateRequest updateRequest) {
+      Optional<Candidate> optionalCandidate = candidateRepository.findById(id);
 
+      if (optionalCandidate.isPresent()) {
+         Candidate toUpdate = optionalCandidate.get();
 
+         toUpdate.setFirstName(updateRequest.getFirstName());
+         toUpdate.setLastName(updateRequest.getLastName());
+         toUpdate.setPosition(updateRequest.getPosition());
+         toUpdate.setPartyAffiliation(updateRequest.getPartyAffiliation());
+
+         candidateRepository.save(toUpdate);
+
+         return "User successfully updated";
+      } else {
+
+         return "User not found with ID: " + id;
+      }
+   }
+   public void deleteById(long CandidateId) {
+      candidateRepository.deleteById(CandidateId);
+   }
 }
