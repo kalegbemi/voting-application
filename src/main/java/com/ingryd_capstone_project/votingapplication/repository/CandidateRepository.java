b2fb646c665aae705069d8ed5a1115dd2ae88045
INVESTHOR365 <azeezyusuf2111@gmail.com>
2024-02-07 13:29:57 +0100

    CandidateRepo modified

--- src/main/java/com/ingryd_capstone_project/votingapplication/repository/CandidateRepository.java
+++ src/main/java/com/ingryd_capstone_project/votingapplication/repository/CandidateRepository.java
@@ -4,14 +4,15 @@ import com.ingryd_capstone_project.votingapplication.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
 
-    Candidate findCandidateById (long id);
-    Candidate findCandidateByFirstName(String firstName);
-    Candidate findCandidateByLastName(String lastName);
-    Candidate findCandidateByPosition(String position);
-    Candidate findCandidateByPartyAffiliation(String partyAffiliation);
+    List<Candidate> findCandidateByFirstName(String firstName);
+    List<Candidate> findCandidateByLastName(String lastName);
+    List<Candidate> findCandidateByPosition(String position);
+    List<Candidate> findCandidateByPartyAffiliation(String partyAffiliation);
 
     long getId();
 }
